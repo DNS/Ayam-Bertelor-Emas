5c0e25b5b76eac2aa500c28f822835868f8aabf6
Daniel Sirait <dsirait@aol.com>
2014-07-16 07:04:24 +0700

    A: update-external-files.ps1

---
+++ update-external-files.ps1
@@ -0,0 +1,5 @@
+
+
+# This is temporary file to copy Infestation.mq4 from MetaEditor working directory
+
+Copy-Item "C:\Users\dns\AppData\Roaming\MetaQuotes\Terminal\A2C1C0A18E65438E4DF1E079A73F6849\MQL4\Experts\Infestation.mq4" "D:\SANDBOX\Expert Advisor\Ayam Bertelor Emas\Infestation.mq4"
